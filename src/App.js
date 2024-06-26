56396e137421bc9387d1dd595aef3673f3bcf960
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-06-27 00:59:36 +0530

    updated drop-down menu

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   const [attendees, setAttendees] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [filterCompany, setFilterCompany] = useState('');
-  const [theme, setTheme] = useState('dark');
   const [sortKey, setSortKey] = useState('name');
   const [sortOrder, setSortOrder] = useState('asc');
   const [companyOptions, setCompanyOptions] = useState([]);
@@ -21,10 +20,6 @@ function App() {
       })
       .catch(error => console.error('Error fetching attendees:', error));
   }, []);
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-  }, [theme]);
 
   const sortedAttendees = [...attendees].sort((a, b) => {
     if (a[sortKey] < b[sortKey]) return sortOrder === 'asc' ? -1 : 1;
@@ -51,7 +46,9 @@ function App() {
   };
 
   return (
+    
     <div className="App">
+     
       <div className='nav-bar'>
         <dotlottie-player src="https://lottie.host/e91ec6ca-28eb-4ff6-afd6-89609dc8ba8c/VONcoIIQ0r.json" background="transparent" speed="1" style={{ width: '100px', height: '100px' }} loop autoplay></dotlottie-player>
         <h1>ReactNexus Attendees</h1>
@@ -67,7 +64,7 @@ function App() {
         value={filterCompany}
         onChange={handleFilterCompany}
       >
-        <option value="">Filter by company...</option>
+        <option value="">All Companies...</option>
         {companyOptions.map((company, index) => (
           <option key={index} value={company}>{company}</option>
         ))}
